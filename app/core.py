eb2d5d3e14576555cdcd2a3781099c1e22efcd1b
momopranto <m.amin@nyu.edu>
2016-12-08 13:53:13 -0500

    routes

--- app/core.py
+++ app/core.py
@@ -5,3 +5,35 @@ web = Blueprint('web', __name__)
 @web.route('/')
 def home():
     return render_template('index.html')
+
+@web.route('/login')
+def login():
+    pass
+
+@web.route('/logout')
+def login():
+    pass
+
+@web.route('/register'):
+def register():
+    pass
+
+
+@web.route('/view')
+def view():
+    pass
+
+
+@web.route('/search')
+def search():
+    pass
+
+
+@web.route('/create')
+def create():
+    pass
+
+
+@web.route('/view')
+def view():
+    pass
